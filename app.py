ba5a6d5301f49c59da9caece6514933f46be14c5
Yuka0530 <152409295+Yuka0530@users.noreply.github.com>
2026-03-07 11:05:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -136,6 +136,7 @@ SPOON_WEIGHT = {
     "油": {"tbsp": 12, "tsp": 4},
     "オリーブオイル": {"tbsp": 12, "tsp": 4},
     "＜でん粉・でん粉製品＞　（でん粉類）　じゃがいもでん粉": {"tbsp": 9, "tsp": 3},
+    "片栗粉": {"tbsp": 9, "tsp": 3},
 }
 
 def get_spoon_weight(food_name, spoon_type):
@@ -352,6 +353,7 @@ if url_text:
                 save_to_gsheet(original, selected)
         
             st.success("Google Sheetsに保存しました！✨")
+
 
 
 
