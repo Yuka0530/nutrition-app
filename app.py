35997fea9e53e69c34edb9d9fcee8bc224891ba5
Yuka0530 <152409295+Yuka0530@users.noreply.github.com>
2026-03-09 10:26:04 +0900

    Update app.py

--- app.py
+++ app.py
@@ -431,7 +431,7 @@ if url_text:
                 amount = st.number_input(
                     "グラム",
                     value=float(display_g),
-                    key=f"{i}_{ing['name']}_amt"
+                    key=f"{i}_{ing['name']}_amt_{multiplier}"
                 )
 
                 
@@ -461,6 +461,7 @@ if url_text:
                 save_to_gsheet(original, selected)
         
             st.success("Google Sheetsに保存しました！✨")
+
 
 
 
